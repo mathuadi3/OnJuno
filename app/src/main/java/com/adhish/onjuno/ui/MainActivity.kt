fcccd1229a313a87046ad1454ba7d14286c4437e
Adhish <mathur.adi3@gmail.com>
2022-05-07 17:46:58 +0530

    feat(): add MainActivity

--- app/src/main/java/com/adhish/onjuno/ui/MainActivity.kt
+++ app/src/main/java/com/adhish/onjuno/ui/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.adhish.onjuno.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.adhish.onjuno.R
+import com.adhish.onjuno.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        navController = Navigation.findNavController(this, R.id.host_fragment)
     }
 }
